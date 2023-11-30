c555413718ae742b822cca7d18335f9484e16381
Christoph Loy <loy.christoph@gmail.com>
2023-11-30 19:53:30 +0100

    Update to JDK 21

--- build-logic/src/main/kotlin/java-convention.gradle.kts
+++ build-logic/src/main/kotlin/java-convention.gradle.kts
@@ -17,7 +17,7 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
