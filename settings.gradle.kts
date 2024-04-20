0621c79ddfef88e2483b564c68d0c656513f1710
Christoph Loy <loy.christoph@gmail.com>
2024-04-20 13:12:44 +0200

    Fix develocity deprecations

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,18 +10,16 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version ("3.17.2")
+    id("com.gradle.develocity") version ("3.17.2")
     id("com.gradle.common-custom-user-data-gradle-plugin") version ("2.0.1")
     id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
 }
 
-gradleEnterprise {
+develocity {
     buildScan {
-        capture {
-            isTaskInputFiles = true
-        }
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = System.getenv("GRADLE_TOS_AGREE")
+        capture.fileFingerprints = true
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = System.getenv("GRADLE_TOS_AGREE")
     }
 }
 
